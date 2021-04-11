ca66ecc4652e6ff12635a8e0059e82619437c5f6
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-04-11 22:31:22 +1200

    PATCH: automated linter

--- src/Forms/GridField/GridFieldSendToBottomAction.php
+++ src/Forms/GridField/GridFieldSendToBottomAction.php
@@ -72,6 +72,7 @@ class GridFieldSendToBottomAction implements GridField_ColumnProvider, GridField
         if ('Actions' === $columnName) {
             return ['title' => ''];
         }
+
         return [];
     }
 
